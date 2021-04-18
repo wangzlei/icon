56ae8abde97bb2d449fec289c8304cd0ead965dd
Wang <wangzl@amazon.com>
2021-04-18 16:20:05 -0700

    update get layer arn script

--- adot/python/layerArn.sh
+++ adot/python/layerArn.sh
@@ -1,2 +1,3 @@
 region=${AWS_REGION-$(aws configure get region)}
-cat release-note.md | grep $region | sed "s/[ ]//g;s/\|//g" | sed "s/^$region//"
+layerArn=$(cat release-note.md | grep $region | sed "s/[ ]//g;s/\|//g" | sed "s/^$region//")
+echo $layerArn
